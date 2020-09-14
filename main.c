9112578f64de612daa22b87624088dc3ccda8543
moisesmtzs <moises.martinez6605@alumnos.udg.mx>
2020-09-14 11:03:23 -0500

    Capturar edad e imprimir edad a cumplir

--- main.c
+++ main.c
@@ -4,11 +4,15 @@
 int main(){
 
     char nombre[150];
+    unsigned int edad;
 
     printf("Hello World\n");
-    printf("Nombre:  ");
+    printf("Nombre: ");
     fgets(nombre,sizeof(nombre),stdin);
     printf("Hola %s", nombre);
+    printf("Edad: ");
+    scanf("%u",&edad);
+    printf("Vas a cumplir %u", edad+1);
 
     return 0;
 
